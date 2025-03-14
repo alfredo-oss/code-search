04a4340bfc478c5e60da0b9a1b89df6b94e0d75f
alfredo-oss <alfredo.derodt@gmail.com>
2025-03-14 08:13:33 -0300

    added a little frontend for file editing

---
+++ codesearch-front/src/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+function CodeEditor () {
+
+}
+
+function App() {
+
+  return (
+    <>
+      hola
+    </>
+  )
+}
+
+export default App
